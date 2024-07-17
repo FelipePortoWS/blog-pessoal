f0fb8dfd2d8022ecc1f4ad7fa92894048957324b
Felipe Porto <fe2017li@gmail.com>
2024-07-17 17:59:36 -0300

    .

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -38,6 +38,10 @@ private LocalDateTime data;
 @JsonIgnoreProperties("postagem")
 private Tema tema;
 
+@ManyToOne
+@JsonIgnoreProperties("postagem")
+private Usuario usuario;
+
 public Long getId() {
 	return id;
 }
@@ -78,6 +82,12 @@ public void setTema(Tema tema) {
 	this.tema = tema;
 }
 
+public Usuario getUsuario() {
+	return usuario;
+}
 
+public void setUsuario(Usuario usuario) {
+	this.usuario = usuario;
+}
 
 }
